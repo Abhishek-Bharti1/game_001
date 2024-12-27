49191f23e36bf220dacdc600699631b2045eed89
Abhishek-Bharti1 <abhishekbharti91550@gmail.com>
2024-12-27 11:38:19 +0530

    issues fixed.

--- src/pages/StartGame.jsx
+++ src/pages/StartGame.jsx
@@ -1,7 +1,9 @@
+import useSound from "use-sound";
 import Button from "../components/Button";
-
+import CorrectSound from "../assets/correct.mp3";
 // eslint-disable-next-line react/prop-types
 const StartGame = ({ handleShowGame }) => {
+  const [playSound] = useSound(CorrectSound);
   return (
     <div className="h-screen w-screen flex relative">
       <div className="w-1/12 bg-[#1a1333]"></div>
@@ -19,6 +21,10 @@ const StartGame = ({ handleShowGame }) => {
       <div className="absolute top-[45%] left-[45%]">
         <Button handleShowGame={handleShowGame} />
       </div>
+      <div className="absolute top-6 right-8 cursor-pointer">
+      <button onClick={()=>playSound()} className="p-2 text-white font-butterfly-kids brand-linear-gr rounded-lg text-lg">Check Sound 🔊</button>
+      </div>
+      
     </div>
   );
 };
